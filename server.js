d9d299cf7fb2898e541a6efb03da00e8a33a110c
Anna Olson <aolson32@css.edu>
2023-03-06 14:44:36 -0600

    modified:   server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import movies from './api/movies.route.js'
 
 const app = express()
+const port = 5000;
 
 app.use(cors())
 app.use(express.json())
@@ -11,5 +12,7 @@ app.use("/api/v1/movies", movies)
 app.use('*', (req,res)=>{
     res.status(404).json({error: "not found"})
 })
+
+app.listen(port, console.log(`Server started on port: ${port}`))
 
 export default app
